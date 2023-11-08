065ae7d01e71ff9dc1704ec61bf1ae17d33b8589
tliam1 <tliam@hawaii.edu>
2023-11-07 21:10:56 -0600

    removing stuffs from API folder

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,27 +1,10 @@
 import { Meteor } from 'meteor/meteor';
-import { Stuffs } from '../../api/stuff/Stuff.js';
 import { Contacts } from '../../api/contact/contact';
-
-/* eslint-disable no-console */
-
-// Initialize the database with a default data document.
-const addData = (data) => {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.collection.insert(data);
-};
 
 const addContactsData = (data) => {
   console.log(`  Adding: ${data.lastName} (${data.owner})`);
   Contacts.collection.insert(data);
 };
-
-// Initialize the StuffsCollection if empty.
-if (Stuffs.collection.find().count() === 0) {
-  if (Meteor.settings.defaultData) {
-    console.log('Creating default data.');
-    Meteor.settings.defaultData.forEach(data => addData(data));
-  }
-}
 
 if (Contacts.collection.find().count() === 0) {
   if (Meteor.settings.defaultContacts) {
